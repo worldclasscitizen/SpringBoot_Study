a0193cc35992c7f2d6ebb411ced91f2ce43917be
teddynu <jk62362@mensakorea.org>
2024-08-09 04:31:52 +0900

    Added Model type parameter & attribute 'username'

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,4 +1,16 @@
 package com.example.firstproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        // model 객체가 "진우" 값을 "username" 에 연결해 웹 브라우저로 보낸다.
+        model.addAttribute("username", "Jinwoo");
+        return "greetings"; // greetings.mustsache 파일 반환
+    }
+
 }
