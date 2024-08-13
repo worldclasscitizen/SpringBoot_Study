4633aef3d42399e11733f3f106a2a5068323cdcb
teddynu <jk62362@mensakorea.org>
2024-08-14 05:45:00 +0900

    Created Entity package

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,8 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         System.out.println(form.toString());
+        // 1. DTO 를 Entity 로 변환
+        Article article = form.toEntity();
         return "";
     }
 }
---
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,4 @@
+package com.example.firstproject.entity;
+
+public class Article {
+}
