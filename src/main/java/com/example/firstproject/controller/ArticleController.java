df43cd45dbfa972d76fab7c23458f3fdbdcc1a28
teddynu <jk62362@mensakorea.org>
2024-08-15 01:50:09 +0900

    Created ArticleRepository & Saved entity to Database with CrudRepository.save() method

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -2,12 +2,14 @@ package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+    private ArticleRepository articleRepository;
     @GetMapping("/articles/new")
     public String newArticleForm() {
         return "articles/new";
@@ -18,9 +20,8 @@ public class ArticleController {
         System.out.println(form.toString());
         // 1. DTO 를 Entity 로 변환
         Article article = form.toEntity();
+        // 2. Repository 로 Entity 를 DB 에 저장
+        Article saved = articleRepository.save(article);
         return "";
     }
 }
-
-
-
