f9e233e0dc77997fc2257cfabd733be7406b2b2e
teddynu <jk62362@mensakorea.org>
2024-08-24 04:54:45 +0900

    Added Service class (not completed)

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -15,56 +15,56 @@ import java.util.List;
 @RestController
 public class ArticleApiController {
     @Autowired
-    private ArticleRepository articleRepository;
-    // GET
-    @GetMapping("/api/articles")
-    public List<Article> index() {
-        return articleRepository.findAll();
-    }
-
-    @GetMapping("/api/articles/{id}")
-    public Article show(@PathVariable Long id) {
-        return articleRepository.findById(id).orElse(null);
-    }
-
-    // POST
-    @PostMapping("/api/articles")
-    public Article create(@RequestBody ArticleForm form) {
-        Article article = form.toEntity();
-        return articleRepository.save(article);
-    }
-    // PATCH
-    @PatchMapping("/api/articles/{id}")
-    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm form) {
-        // 1. 수정용 엔티티 생성하기
-        Article article = form.toEntity();
-        log.info("id : {}, article : {}", id, article.toString());
-        log.info("form : " + form.toString());
-
-        // 2. DB 에 대상 엔티티가 있는지 조회하기
-        Article target = articleRepository.findById(id).orElse(null);
-
-        // 3. 대상 엔티티가 없거나, 수정하려는 id 가 잘못됐을 경우 처리하기
-        if(target == null || id != article.getId()) {
-            log.info("잘못된 요청! id : {}, article : {}", id, article.toString());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        // 4. 대상 엔티티가 있으면 수정 내용으로 업데이트 후 상태 코드 200 보내기
-        target.patch(article);
-        Article updated = articleRepository.save(target);
-        return ResponseEntity.status(HttpStatus.OK).body(updated);
-    }
-    // DELETE
-    @DeleteMapping("/api/articles/{id}")
-    public ResponseEntity<Article> delete(@PathVariable Long id) {
-        // 1. 대상 엔티티 조회
-        Article target = articleRepository.findById(id).orElse(null);
-        // 2. 대상 엔티티가 없을 때
-        if(target == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        // 3. 대상 엔티티가 있을 때
-        articleRepository.delete(target);
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
+    private ArticleService articleService;
+//    // GET
+//    @GetMapping("/api/articles")
+//    public List<Article> index() {
+//        return articleRepository.findAll();
+//    }
+//
+//    @GetMapping("/api/articles/{id}")
+//    public Article show(@PathVariable Long id) {
+//        return articleRepository.findById(id).orElse(null);
+//    }
+//
+//    // POST
+//    @PostMapping("/api/articles")
+//    public Article create(@RequestBody ArticleForm form) {
+//        Article article = form.toEntity();
+//        return articleRepository.save(article);
+//    }
+//    // PATCH
+//    @PatchMapping("/api/articles/{id}")
+//    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm form) {
+//        // 1. 수정용 엔티티 생성하기
+//        Article article = form.toEntity();
+//        log.info("id : {}, article : {}", id, article.toString());
+//        log.info("form : " + form.toString());
+//
+//        // 2. DB 에 대상 엔티티가 있는지 조회하기
+//        Article target = articleRepository.findById(id).orElse(null);
+//
+//        // 3. 대상 엔티티가 없거나, 수정하려는 id 가 잘못됐을 경우 처리하기
+//        if(target == null || id != article.getId()) {
+//            log.info("잘못된 요청! id : {}, article : {}", id, article.toString());
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+//        }
+//        // 4. 대상 엔티티가 있으면 수정 내용으로 업데이트 후 상태 코드 200 보내기
+//        target.patch(article);
+//        Article updated = articleRepository.save(target);
+//        return ResponseEntity.status(HttpStatus.OK).body(updated);
+//    }
+//    // DELETE
+//    @DeleteMapping("/api/articles/{id}")
+//    public ResponseEntity<Article> delete(@PathVariable Long id) {
+//        // 1. 대상 엔티티 조회
+//        Article target = articleRepository.findById(id).orElse(null);
+//        // 2. 대상 엔티티가 없을 때
+//        if(target == null) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+//        }
+//        // 3. 대상 엔티티가 있을 때
+//        articleRepository.delete(target);
+//        return ResponseEntity.status(HttpStatus.OK).build();
+//    }
 }
--- src/main/java/com/example/firstproject/entity/Coffee.java
+++ src/main/java/com/example/firstproject/entity/Coffee.java
@@ -28,5 +28,4 @@ public class Coffee {
             this.price = coffee.price;
         }
     }
-
 }
