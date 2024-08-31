eb01ccd520e4d6f5ef2958460c64cbcce58edee1
Jinwoo Choi <jk62362@mensakorea.org>
2024-08-31 09:56:58 +0900

    Added Comment Entity

--- src/test/java/com/example/firstproject/service/ArticleServiceTest.java
+++ src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -55,10 +55,10 @@ class ArticleServiceTest {
     @Transactional
     @Test
     void create_success() {
-        String title = "라라라라";
-        String content = "4444";
+        String title = "DB에 담겨 있는 제목";
+        String content = "DB에 담겨 있는 내용";
         ArticleForm form = new ArticleForm(null, title, content);
-        Article expected = new Article(4L, "라라라라", "4444");
+        Article expected = new Article(4L, "DB에 담겨 있는 제목", "DB에 담겨 있는 내용");
 
         Article article = articleService.create(form);
 
@@ -75,6 +75,70 @@ class ArticleServiceTest {
         Article expected = null;
 
         Article article = articleService.create(form);
+
+        assertEquals(expected, article);
+    }
+
+    @Test
+    @Transactional
+    void update_all() {
+        Long id = 3L;
+        String title = "파파파파";
+        String content = "1234";
+        ArticleForm form = new ArticleForm(id, title, content);
+        Article expected = new Article(id, title, content);
+
+        Article article = articleService.update(id, form);
+
+        assertEquals(expected.toString(), article.toString());
+    }
+
+    @Test
+    @Transactional
+    void update_idTitle() {
+        Long id = 3L;
+        String title = "파파파파";
+        String content = null;
+        ArticleForm form = new ArticleForm(id, title, content);
+        Article expected = new Article(3L, "파파파파", "3333");
+
+        Article article = articleService.update(id, form);
+
+        assertEquals(expected.toString(), article.toString());
+    }
+
+    @Test
+    @Transactional
+    void update_fail() {
+        Long id = 5L;
+        String title = "5번 게시물은";
+        String content = "없습니다.";
+        ArticleForm form = new ArticleForm(id, title, content);
+        Article expected = null;
+
+        Article article = articleService.update(id, form);
+
+        assertEquals(expected, article);
+    }
+
+    @Test
+    @Transactional
+    void delete_success() {
+        Long id = 2L;
+        Article expected = new Article(id, "나나나나", "2222");
+
+        Article article = articleService.delete(id);
+
+        assertEquals(expected.toString(), article.toString());
+    }
+
+    @Test
+    @Transactional
+    void delete_fail() {
+        Long id = -1L;
+        Article expected = null;
+
+        Article article = articleService.delete(id);
 
         assertEquals(expected, article);
     }
