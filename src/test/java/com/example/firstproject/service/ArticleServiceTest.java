f76f05d9c216b331a63c0851e93e53b285e12b32
Jinwoo Choi <jk62362@mensakorea.org>
2024-09-02 19:45:32 +0900

    Added some test cases for CommentRepository

--- src/test/java/com/example/firstproject/service/ArticleServiceTest.java
+++ src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -25,7 +25,7 @@ class ArticleServiceTest {
         Article a = new Article(1L, "가가가가", "1111");
         Article b = new Article(2L, "나나나나", "2222");
         Article c = new Article(3L, "다다다다", "3333");
-        List<Article> expected = new ArrayList<Article>(Arrays.asList(a, b, c));
+        List<Article> expected = new ArrayList<>(Arrays.asList(a, b, c));
         // 2. 실제 데이터
         List<Article> articles = articleService.index();
         // 3. 비교 및 검증
